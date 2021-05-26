4e57ae57149523fb8b280ab30d542bd0a8785701
Hundredl <sagacious666@163.com>
2021-05-26 13:24:52 +0800

    feat:/jpa/teacher/update

---
+++ src/main/java/javaee/jpa/two/servlet/TeacherUpdate.java
@@ -0,0 +1,42 @@
+package javaee.jpa.two.servlet;
+
+import javaee.jpa.two.entity.Course;
+import javaee.jpa.two.entity.Teacher;
+import javaee.jpa.two.service.TeacherService;
+import javaee.jpa.two.service.TeacherServiceImp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "TeacherUpdate", value = "/jpa/teacher/update")
+public class TeacherUpdate extends HttpServlet {
+    static TeacherService teacherService=new TeacherServiceImp();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        int id=Integer.parseInt(req.getParameter("id"));
+        Teacher teacher=new Teacher(id,Integer.parseInt(req.getParameter("age")),req.getParameter("gender"),req.getParameter("name"),new Course(1,req.getParameter("course")));
+        teacherService.update(id,teacher);
+        //返回结果
+        PrintWriter out = resp.getWriter();
+        List<Teacher> result = teacherService.findAll();
+        out.println("id\t gender \t age \t name \t course");
+        out.println("<br>");
+        for (Teacher m: result) {
+            out.println(m.getId() + "\t" + m.getGender() + "\t" + m.getAge() + "\t"+ m.getTeacherName()+"\t"+m.getCourse().getCourseName());
+            out.println("<br>");
+        }
+    }
+}
