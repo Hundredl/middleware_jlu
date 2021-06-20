42ad1dc06809a7d780d290baa4c75cf5764163e3
Hundredl <sagacious666@163.com>
2021-06-20 23:34:31 +0800

    :sparkles:/book/user/order/insertTemp 向购物车中添加书籍，数目<0移除

--- src/main/java/javaee/book/service/OrderStatefulService.java
+++ src/main/java/javaee/book/service/OrderStatefulService.java
@@ -37,23 +37,40 @@ public class OrderStatefulService {
      */
     public void addToSubOrderList(Integer bookId,Integer num)
     {
+        BookSubOrder destSubOrder=null;
+        //找到需要修改的那个子订单
         for (BookSubOrder subOrder:subOrderList)
         {
             if (subOrder.getBookBook().getBookId().equals(bookId))
             {
-                subOrder.setNum(num);
-                return;
+                //subOrder.setNum(num);
+                destSubOrder=subOrder;
             }
         }
-        BookBook book=bookDao.selectById(bookId);
-        if (book!=null)
+        if (destSubOrder!=null)
         {
-            BookSubOrder bookSubOrder=new BookSubOrder();
-            bookSubOrder.setBookBook(book);
-            bookSubOrder.setNum(num);
-            bookSubOrder.setPrice(getBookPrice(book));
-            subOrderList.add(bookSubOrder);
+            //说明此时需要更新
+            if (num<=0)
+            {
+                subOrderList.remove(destSubOrder);
+            }
+            else {
+                destSubOrder.setNum(num);
+            }
         }
+        else {
+            //说明此时需要添加书籍
+            BookBook book=bookDao.selectById(bookId);//找到书籍的信息
+            if (book!=null&&num>0)//找到书籍信息，并且需要添加。num<=0不添加。没有找到书籍信息不添加
+            {
+                BookSubOrder bookSubOrder=new BookSubOrder();
+                bookSubOrder.setBookBook(book);
+                bookSubOrder.setNum(num);
+                bookSubOrder.setPrice(getBookPrice(book));
+                subOrderList.add(bookSubOrder);
+            }
+        }
+
     }
 
     /**
