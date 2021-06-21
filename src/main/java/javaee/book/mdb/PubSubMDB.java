ecbb74bc9717137c0b17ccd0b228caaac24e74a0
Hundredl <sagacious666@163.com>
2021-06-21 14:24:44 +0800

    :sparkles: MDB调试成功

---
+++ src/main/java/javaee/book/mdb/PubSubMDB.java
@@ -0,0 +1,43 @@
+package javaee.book.mdb;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+//设置消息驱动Bean监听的目的类型为Topic、目的地址为topic/test、可持久性存储消息以及用户ID
+@MessageDriven(activationConfig =  {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "topic/test"),
+        @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
+        @ActivationConfigProperty(propertyName = "clientID", propertyValue = "consumer")
+})
+public class PubSubMDB implements MessageListener {
+
+    @Resource
+    private MessageDrivenContext mdc;
+    public PubSubMDB() {
+    }
+    public void onMessage(Message inMessage) {
+        TextMessage msg = null;
+        try {
+            if (inMessage instanceof TextMessage) {
+                msg = (TextMessage) inMessage;
+                System.out.println("PubSub消息驱动Bean 1 接收到的消息:" + msg.getText());
+            }
+            else {
+                System.out.println("消息的类型不正确： " + inMessage.getClass().getName());
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+            mdc.setRollbackOnly();
+        } catch (Throwable te) {
+            te.printStackTrace();
+        }
+
+    }
+}
--- src/main/java/javaee/book/servlet/user/order/OrderInsertTemporarilyServlet.java
+++ src/main/java/javaee/book/servlet/user/order/OrderInsertTemporarilyServlet.java
@@ -13,23 +13,39 @@ import javaee.book.utils.ServletUtils;
 import lombok.SneakyThrows;
 
 import javax.ejb.EJB;
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @WebServlet(name = "OrderInsertTemporarilyServlet" , value = "/book/user/order/insertTemp")
 public class OrderInsertTemporarilyServlet extends HttpServlet {
+    private static final String DEFAULT_MESSAGE = "Welcome to JMS!";
+    private static final String DEFAULT_CONNECTION_FACTORY = "java:/RemoteConnectionFactory";
+    private static final String DEFAULT_DESTINATION = "java:/queue/test";
+    private static final String DEFAULT_MESSAGE_COUNT = "1";
+    private static final String DEFAULT_USERNAME = "testJNDI";
+    private static final String DEFAULT_PASSWORD = "123";
+
     @EJB
     OrderStatefulService orderStatefulService;
     Map<?,?> params;
     @SneakyThrows
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         //resp.setContentType("application/json;charset=UTF-8");
         params= ServletUtils.getPostParams(req);
         if (params.get("option")==null)
@@ -38,12 +54,16 @@ public class OrderInsertTemporarilyServlet extends HttpServlet {
             return;
         }
         String option=params.get("option").toString();
+        System.out.println(option);
         switch (option){
             case "insertTemp":
                 insertTemp(req,resp);
                 break;
             case "save":
                 save(req, resp);
+                break;
+            case "print":
+                print(req, resp);
                 break;
             default:
                 ServletUtils.returnResp(resp, GlobalVar.RespMsg.wrong_param,null);
@@ -82,6 +102,44 @@ public class OrderInsertTemporarilyServlet extends HttpServlet {
         else {
             //书籍数量足够
             ServletUtils.returnResp(resp, GlobalVar.RespMsg.book_order_insert_temp_save, OrderUtils.getBookSubOrderWithBookList(orderStatefulService.getSubOrderList()));
+        }
+    }
+    public void print(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Context context = null;
+        Connection connection = null;
+        try {
+            // 设置上下文
+            System.out.println("设置JNDI访问环境信息也就是设置应用服务器的上下文信息!");
+            // 获取到InitialContext对象.
+            context = new InitialContext();
+            System.out.println("获取连接工厂!");
+            ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup(DEFAULT_CONNECTION_FACTORY);
+
+            System.out.println("获取目的地!");
+            Destination destination = (Destination) context.lookup(DEFAULT_DESTINATION);
+            // 创建JMS连接、会话、生产者和消费者
+            connection = connectionFactory.createConnection(DEFAULT_USERNAME, DEFAULT_PASSWORD);
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(destination);
+            connection.start();
+            int count = Integer.parseInt(DEFAULT_MESSAGE_COUNT);
+            // 发送特定数目的消息
+            TextMessage message = null;
+            for (int i = 0; i < count; i++) {
+                message = session.createTextMessage(DEFAULT_MESSAGE);
+                producer.send(message);
+                System.out.println("message:" + message);
+                System.out.println("message:" + DEFAULT_MESSAGE);
+            }
+            // 等待30秒退出
+            CountDownLatch latch = new CountDownLatch(1);
+            latch.await(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } catch (JMSException e) {
+            e.printStackTrace();
         }
     }
 }
